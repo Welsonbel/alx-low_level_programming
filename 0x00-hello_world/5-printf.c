15a20d7023c40c0ad5e812cf3e8b6f4d0ff0fe68
root <yassines10e@gmail.com>
2023-03-16 16:15:07 -0700

    fvb

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is main the entry point
+ *
+ * Return: is always 0 yesss
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
