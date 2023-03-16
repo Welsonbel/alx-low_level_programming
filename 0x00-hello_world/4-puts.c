8f6fd8fd1bc64fb0b9414906f82c78faae12bbc6
root <yassines10e@gmail.com>
2023-03-16 16:13:14 -0700

    sdfcvbn

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+/**
+ * main - entry point for c
+ * Return: is always 0 for idk why
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
