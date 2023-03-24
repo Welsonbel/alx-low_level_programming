3b289f74900f57020fb9b5bc0a09864198649dff
root <yassines10e@gmail.com>
2023-03-23 19:56:16 -0700

    sdfsgh

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * positive_or_negative - le bab checks for n word or p word
+ * @i: to chekko
+ * Return: always 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
